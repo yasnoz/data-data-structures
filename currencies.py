33c846d6a37f72d6ebe726d3635a14dc6ce64dc7
Yasin Oz <yasnoz@gmail.com>
2024-07-09 23:58:51 +0200

    Progress on data_structure: currency tests passing 6.25/10

--- currencies.py
+++ currencies.py
@@ -1,11 +1,16 @@
 # pylint: disable=missing-docstring
 
 # TODO: add some currency rates
-RATES = {}
+RATES = {"USDEUR": 0.85, "GBPEUR": 1.13, "CHFEUR": 0.86, "EURGBP": 0.885}
 
 def convert(amount, currency):
     """returns the converted amount in the given currency
     amount is a tuple like (100, "EUR")
     currency is a string
     """
-    pass  # YOUR CODE HERE
+    value, current_currency = amount
+    converter = current_currency + currency
+    if converter in RATES:
+      converted = round(value * RATES[converter])
+      return converted
+    return None
